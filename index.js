ddcbba65cfc847c662d3ad653bd1d15f9b918d9d
William Sinn <w.h.sinn@gmail.com>
2019-05-09 22:20:54 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+let currentUser = 'Context.<anonymous>';
+let welcomeMessage = 'Welcome to Flatbook, Context.<anonymous>!';
+let excitedWelcomeMessage = `WELCOME TO FLATBOOK, CONTEXT.<ANONYMOUS>!`;
+let shortGreeting = 'Welcome, C!'
